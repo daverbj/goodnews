42ebfbf8241beea8eba0154fa32bfb5a3f1861e9
daveai <dave@agileinf.com>
2019-12-31 00:44:02 +0530

    Initial commit

---
+++ src/main/java/com/goodnews/app/ws/goodnews/GoodnewsApplication.java
@@ -0,0 +1,13 @@
+package com.goodnews.app.ws.goodnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodnewsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/goodnews/app/ws/goodnews/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.goodnews.app.ws.goodnews;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GoodnewsApplication.class);
+	}
+
+}
